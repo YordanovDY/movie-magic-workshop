657107a24ae1c35e165b7f9f936adb283597efc2
YordanovDY <yordanovdy23@gmail.com>
2025-01-16 14:16:04 +0200

    Setup Express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Works!');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Works!');
+});
+
+app.listen(port, () => console.log(`Server is listening on http://localhost:${port}...`));
