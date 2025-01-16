d214f589a96cf3b471d9ef4977dcc8fee09dad31
YordanovDY <yordanovdy23@gmail.com>
2025-01-16 16:25:05 +0200

    Add dynamic movie rendering on home page
    Delete useless comments

--- src/index.js
+++ src/index.js
@@ -16,20 +16,8 @@ app.use('/static', express.static('./src/public'));
 
 app.use(routes);
 
-// app.get('/', (req, res) => {
-//     res.render('home');
-// });
-
-// app.get('/about', (req, res) => {
-//     res.render('about');
-// });
-
 // app.get('/create', (req, res) => {
 //     res.render('create');
-// });
-
-// app.get('*', (req, res) => {
-//     res.render('404');
 // });
 
 app.listen(port, () => console.log(`Server is listening on http://localhost:${port}...`));
---
+++ src/movies.js
@@ -0,0 +1,24 @@
+export default [
+    {
+        id: 1,
+        title: "Jungle Cruise",
+        category: "Movie",
+        genre: "Fantasy",
+        director: "Jaume Collet-Serra",
+        year: 2021,
+        imageURL: "/static/img/jungle-cruise.jpeg",
+        rating: 6.6,
+        description: "Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans." 
+    },
+    {
+        id: 2,
+        title: "The Little Mermaid",
+        category: "Movie",
+        genre: "Musical",
+        director: "Rob Marshall",
+        year: 2023,
+        imageURL: "/static/img/the-little-mermaid.jpg",
+        rating: 7.2,
+        description: "The youngest of King Triton's daughters, Ariel is a beautiful and spirited young mermaid with a thirst for adventure. Longing to find out more about the world beyond the sea, Ariel visits the surface and falls for the dashing Prince Eric. Following her heart, she makes a deal with the evil sea witch, Ursula, to experience life on land." 
+    }
+];
