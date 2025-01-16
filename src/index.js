55da8d141e8c758043a47f365c6b8945509552fc
YordanovDY <yordanovdy23@gmail.com>
2025-01-16 14:38:07 +0200

    Setup handlebars view engine

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
 const port = 3000;
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-    res.send('Works!');
+    res.render('test', {layout: false});
 });
 
 app.listen(port, () => console.log(`Server is listening on http://localhost:${port}...`));
