089260398c9005acb712ab3453f7393f305d7699
YordanovDY <yordanovdy23@gmail.com>
2025-01-17 14:36:44 +0200

    Add details page for each movie

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
+import movies from '../movies.js';
 
 const movieController = Router();
 
 movieController.get('/create', (req, res) => {
     res.render('create');
 });
+
+movieController.get('/:movieId/details', (req, res) => {
+    const movieId = req.params.movieId;
+    const movie = movies.find(m => m.id === movieId);
+    console.log(movie);
+    
+
+    res.render('details', { movie });
+})
 
 export default movieController;
--- src/movies.js
+++ src/movies.js
@@ -1,6 +1,6 @@
 export default [
     {
-        id: 1,
+        id: '1',
         title: "Jungle Cruise",
         category: "Movie",
         genre: "Fantasy",
@@ -11,7 +11,7 @@ export default [
         description: "Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans." 
     },
     {
-        id: 2,
+        id: '2',
         title: "The Little Mermaid",
         category: "Movie",
         genre: "Musical",
