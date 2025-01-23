ec66a56ca4bb9fdb68be8c586d06536c8521445f
YordanovDY <yordanovdy23@gmail.com>
2025-01-23 11:44:39 +0200

    Setup Mongoose

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import express from 'express';
 import handlebars from 'express-handlebars';
 import routes from './routes.js';
 import showRatingHelper from './helpers/rating-helper.js';
+import mongoose from 'mongoose';
+import 'dotenv/config'
 
 const app = express();
 const port = 3000;
+const { DATABASE_URI } = process.env;
 
+// DB Config
+try {
+    await mongoose.connect(DATABASE_URI);
+    console.log('DB Connected Successfully.');
+
+} catch (err) {
+    console.error('Cannot connect to DB:\n' + err.message);
+
+}
+
+// Handlebars Config
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
     helpers: {
@@ -16,6 +30,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
+
+// Express Config
 app.use('/static', express.static('./src/public'));
 app.use(express.urlencoded({ extended: false }));
 
