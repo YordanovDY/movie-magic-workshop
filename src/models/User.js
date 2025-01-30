932454b5ecc1cfa4e78c92b62436163817dee8dd
YordanovDY <yordanovdy23@gmail.com>
2025-01-30 14:10:14 +0200

    Hash password on register

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,18 @@
 import { Schema, model } from "mongoose";
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
     email: String,
     password: String
 });
+
+userSchema.pre('save', async function () {
+    // TODO: fix update user bug
+    
+    if (this.password.length < 60) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+})
 
 const User = model('User', userSchema);
 
