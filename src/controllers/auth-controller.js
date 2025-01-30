7678fd42a4e37c1d4a819a8260791e7b9f318f13
YordanovDY <yordanovdy23@gmail.com>
2025-01-30 13:05:45 +0200

    Create login and register page

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -3,11 +3,11 @@ import { Router } from "express";
 const authController = Router();
 
 authController.get('/login', (req, res) => {
-
+    res.render('auth/login');
 });
 
 authController.get('/register', (req, res) => {
-
+    res.render('auth/register');
 });
 
 export default authController;
