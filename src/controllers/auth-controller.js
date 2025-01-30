1baee39afc1276714228980e4e74690adba19864
YordanovDY <yordanovdy23@gmail.com>
2025-01-30 12:50:09 +0200

    Add auth controller

---
+++ src/controllers/auth-controller.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const authController = Router();
+
+authController.get('/login', (req, res) => {
+
+});
+
+authController.get('/register', (req, res) => {
+
+});
+
+export default authController;
