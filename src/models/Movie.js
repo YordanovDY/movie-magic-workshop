a60f7a8680e9b9d6919c0292fe767d51cb8a8920
YordanovDY <yordanovdy23@gmail.com>
2025-02-04 12:09:24 +0200

    Add movie validations

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,14 +1,60 @@
 import { Schema, model, Types } from 'mongoose';
 
 const movieSchema = new Schema({
-    title: String,
-    category: String,
-    genre: String,
-    director: String,
-    year: Number,
-    imageUrl: String,
-    rating: Number,
-    description: String,
+    title: {
+        type: String,
+        required: [true, 'Title is required!'],
+        minLength: [5, 'Title must be at least 5 characters long!'],
+        match: [/^[a-zA-z 0-9]+$/, 'Title must contain alphanumeric and whitespaces only!']
+    },
+    category: {
+        type: String,
+        enum: [
+            'tv-show',
+            'animation',
+            'movie',
+            'documentary',
+            'short-film'
+        ]
+    },
+    genre: {
+        type: String,
+        required: [true, 'Genre is required!'],
+        minLength: [5, 'Genre must be at least 5 characters long!'],
+        match: [/^[a-zA-z 0-9]+$/, 'Genre must contain alphanumeric and whitespaces only!']
+    },
+    director: {
+        type: String,
+        required: [true, 'Director is required!'],
+        minLength: [5, 'Director must be at least 5 characters long!'],
+        match: [/^[a-zA-z 0-9]+$/, 'Director must contain alphanumeric and whitespaces only!']
+    },
+    year: {
+        type: Number,
+        required: [true, 'Year is required!'],
+        min: [1900, 'Year must be in range 1900 to 2025'],
+        max: [2025, 'Year must be in range 1900 to 2025']
+    },
+    imageUrl: {
+        type: String,
+        validate: {
+            validator: function (v) {
+                const condition = v.startsWith('http') || v.startsWith('https');
+                return condition;
+            },
+            message: (props) => 'Invalid image URL'
+        }
+    },
+    rating: {
+        type: Number,
+        min: [1, 'Rating must be in range 1 to 10'],
+        max: [10, 'Rating must be in range 1 to 10']
+    },
+    description: {
+        type: String,
+        minLength: [20, 'Description must be at least 20 characters long!'],
+        match: [/^[a-zA-z 0-9]+$/, 'Description must contain alphanumeric and whitespaces only!']
+    },
     casts: [{
         type: Types.ObjectId,
         ref: 'Cast'
