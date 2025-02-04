8dc3912eb5c6dce320387fd58e75e182d8abff54
YordanovDY <yordanovdy23@gmail.com>
2025-02-04 12:18:34 +0200

    Add casts validations

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -1,10 +1,33 @@
 import { Schema, model } from "mongoose";
 
 const castSchema = new Schema({
-    name: String,
-    age: Number,
-    born: String,
-    imageUrl: String,
+    name: {
+        type: String,
+        required: [true, 'Name is required!'],
+        minLength: [5, 'Name must be at least 5 characters long!'],
+        match: [/^[a-zA-z 0-9]+$/, 'Name must contain alphanumeric and whitespaces only!']
+    },
+    age: {
+        type: Number,
+        min: 1,
+        max: 120
+    },
+    born: {
+        type: String,
+        required: [true, 'Born is required!'],
+        minLength: [10, 'Born must be at least 10 characters long!'],
+        match: [/^[a-zA-z 0-9]+$/, 'Born must contain alphanumeric and whitespaces only!']
+    },
+    imageUrl: {
+        type: String,
+        validate: {
+            validator: function (v) {
+                const condition = v.startsWith('http') || v.startsWith('https');
+                return condition;
+            },
+            message: (props) => 'Invalid image URL'
+        }
+    },
 });
 
 const Cast = model('Cast', castSchema);
